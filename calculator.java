412389e1230b7623e178463884200f6c080e3251
kimdaegeunn <rms-_-@naver.com>
2023-12-13 16:55:35 +0900

    김대근 작업

---
+++ calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("간단한 콘솔 계산기 프로그램");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        double num1 = scanner.nextDouble();
+//김대근 작업추가 
