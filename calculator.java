a470eb69fae9a456dc87ee8064a5ce11ac2aa8b1
UHB4 <gusqls1658@naver.com>
2023-12-13 16:59:12 +0900

    현빈작업

--- calculator.java
+++ calculator.java
@@ -8,3 +8,27 @@ public class Calculator {
         System.out.print("첫 번째 숫자를 입력하세요: ");
         double num1 = scanner.nextDouble();
 //김대근 작업추가 
+//        System.out.print("두 번째 숫자를 입력하세요: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("사용할 연산자를 입력하세요 (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("0으로 나눌 수 없습니다.");
+//엄현빈 작업추가
